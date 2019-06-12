f58c9449b3555d943eeaa764c1cbfc8525308797
RaRo0 <51163489+RaRo0@users.noreply.github.com>
2019-06-12 17:13:16 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,10 +1,13 @@
 import SimpleHTTPServer
 import SocketServer
-with open('conf') as f:
-    first_line = f.readline()
-PORT = int(first_line)
-
-Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
-SocketServer.TCPServer.allow_reuse_address = True
-httpd = SocketServer.TCPServer(("127.0.0.1", PORT), Handler)
-httpd.serve_forever()
+try:
+    with open('conf') as f:
+        first_line = f.readline()
+    PORT = int(first_line)
+    
+    Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+    SocketServer.TCPServer.allow_reuse_address = True
+    httpd = SocketServer.TCPServer(("127.0.0.1", PORT), Handler)
+    httpd.serve_forever()
+except:
+    print "error"
